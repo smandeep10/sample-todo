5ee2bcbbe6d94adfc7fe30f66518ce5ddd5f5fb8
Mandeep Singh <mandeep@courses.ninja>
2016-06-13 09:15:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var session = require('cookie-session'); // Loads the piece of middleware for sessions
+var ejs = require('ejs')
+var bodyParser = require('body-parser'); // Loads the piece of middleware for managing the settings
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var app = express();
+
+
+/* Using sessions */
+app.use(session({secret: 'todotopsecret'}))
+
+
+/* If there is no to do list in the session, 
+we create an empty one in the form of an array before continuing */
+app.use(function(req, res, next){
+    if (typeof(req.session.todolist) == 'undefined') {
+        req.session.todolist = [];
+    }
+    next();
+})
+
+/* The to do list and the form are displayed */
+app.get('/todo', function(req, res) { 
+    res.render('todo.ejs', {todolist: req.session.todolist});
+})
+
+/* Adding an item to the to do list */
+app.post('/todo/add/', urlencodedParser, function(req, res) {
+    if (req.body.newtodo != '') {
+        req.session.todolist.push(req.body.newtodo);
+    }
+    res.redirect('/todo');
+})
+
+/* Deletes an item from the to do list */
+.get('/todo/delete/:id', function(req, res) {
+    if (req.params.id != '') {
+        req.session.todolist.splice(req.params.id, 1);
+    }
+    res.redirect('/todo');
+})
+
+/* Redirects to the to do list if the page requested is not found */
+.use(function(req, res, next){
+    res.redirect('/todo');
+})
+
+.listen(8080);
